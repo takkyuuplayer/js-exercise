8814d25087a186f52d8d3610177be9ea555addf1
takkyuuplayer <takkyuuplayer@gmail.com>
2016-12-21 02:15:20 +0900

    Fix test

--- test/lib/circle.js
+++ test/lib/circle.js
@@ -4,9 +4,13 @@ function Circle(radious) {
   this.radious = radious;
 }
 
-Circle.prototype.area = () => this.radious * PI * PI;
+Circle.prototype.area = function area() {
+  return this.radious * PI * PI;
+};
 
-Circle.prototype.circumference = () => this.radious * 2 * PI;
+Circle.prototype.circumference = function circumference() {
+  return this.radious * 2 * PI;
+};
 
 module.exports = Circle;
 
