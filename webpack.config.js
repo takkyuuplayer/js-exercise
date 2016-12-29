b6e255ea3bc16f2200f3b98a6fb7da019e7833f2
takkyuuplayer <takkyuuplayer@gmail.com>
2016-12-29 12:13:56 +0900

    Fix config

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,33 @@
 const path = require('path');
-const output_dir = path.join(__dirname, './public/redux/js');
+
+const outputDir = path.join(__dirname, './public/redux/js');
 
 module.exports = {
   entry: {
     reduxCounter: path.join(__dirname, './src/redux/counter/index.jsx'),
-    reduxTodos:   path.join(__dirname, './src/redux/todos/index.jsx'),
+    reduxTodos: path.join(__dirname, './src/redux/todos/index.jsx'),
   },
   output: {
-    path: output_dir,
-    filename: '[name].bundle.js'
+    path: outputDir,
+    filename: '[name].bundle.js',
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['', '.js', '.jsx'],
   },
   module: {
     loaders: [
       {
         test: /\.jsx?/,
         loader: 'babel-loader',
-        presets: ['es2015', 'react']
-      }
-    ]
+        presets: ['es2015', 'react'],
+      },
+    ],
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   externals: [
     {
       window: 'window',
       document: 'document',
-    }
-  ]
+    },
+  ],
 };
