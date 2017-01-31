ffcace3d5f0826955b45755adb5e4509399eaf4a
takkyuuplayer <takkyuuplayer@gmail.com>
2017-01-31 11:43:09 +0900

    Add test for urijs

--- test/urijs.spec.js
+++ test/urijs.spec.js
@@ -2,7 +2,7 @@ const assert = require('power-assert');
 const urijs = require('urijs');
 
 describe('urijs', () => {
-  describe('query', () => {
+  describe('.query', () => {
     it('can add query parameters', () => {
       const uri = urijs('https://github.com/login/oauth/authorize').query({
         client_id: 12345,
@@ -10,6 +10,14 @@ describe('urijs', () => {
         state: 'abcdefg',
       });
       assert.equal(uri.toString(), 'https://github.com/login/oauth/authorize?client_id=12345&scope=user%3Aemail&state=abcdefg');
+    });
+    it('can generate query string', () => {
+      const uri = urijs('').query({
+        client_id: 12345,
+        scope: 'user:email',
+        state: 'abcdefg',
+      });
+      assert.equal(uri.toString(), '?client_id=12345&scope=user%3Aemail&state=abcdefg');
     });
   });
 });
