f5ebabfea77ec2411cf007de1b1b4e64bcf51e14
takkyuuplayer <takkyuuplayer@gmail.com>
2017-02-06 22:33:32 +0900

    Add test for urijs

--- test/urijs.spec.js
+++ test/urijs.spec.js
@@ -20,4 +20,42 @@ describe('urijs', () => {
       assert.equal(uri.toString(), '?client_id=12345&scope=user%3Aemail&state=abcdefg');
     });
   });
+  describe('.parse', () => {
+    it('should parse uri', () => {
+      const res = urijs.parse('https://github.com/login/oauth/authorize?client_id=12345&scope=user%3Aemail&state=abcdefg');
+      assert.deepStrictEqual(res,
+        { query: 'client_id=12345&scope=user%3Aemail&state=abcdefg',
+          protocol: 'https',
+          username: null,
+          password: null,
+          hostname: 'github.com',
+          port: null,
+          path: '/login/oauth/authorize' },
+    );
+    });
+  });
+  describe('.parseQuery', () => {
+    it('should parse query', () => {
+      assert.deepStrictEqual(
+        urijs.parseQuery('https://github.com/login/oauth/authorize?client_id=12345&scope=user%3Aemail&state=abcdefg'),
+        { 'https://github.com/login/oauth/authorize?client_id': '12345',
+          scope: 'user:email',
+          state: 'abcdefg' },
+      );
+
+      assert.deepStrictEqual(
+        urijs.parseQuery('?client_id=12345&scope=user%3Aemail&state=abcdefg'),
+        { client_id: '12345',
+          scope: 'user:email',
+          state: 'abcdefg' },
+      );
+
+      assert.deepStrictEqual(
+        urijs.parseQuery('client_id=12345&scope=user%3Aemail&state=abcdefg'),
+        { client_id: '12345',
+          scope: 'user:email',
+          state: 'abcdefg' },
+      );
+    });
+  });
 });
