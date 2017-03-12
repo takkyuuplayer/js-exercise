d78b0372ff780d1e1e2fc42c08a9836e3cda7331
takkyuuplayer <takkyuuplayer@gmail.com>
2017-03-12 18:41:58 +0900

    Add routes

--- test/express.spec.js
+++ test/express.spec.js
@@ -14,8 +14,36 @@ describe('exporess', () => {
 
       const body = await request('http://localhost:3000');
 
-      assert(body === 'Hello World!');
+      assert.strictEqual(body, 'Hello World!');
     });
+  });
+
+  describe('routes', () => {
+    describe('.all', () => {
+      const app = express();
+      app.all('/', (req, res, next) => {
+        /* eslint-disable no-param-reassign */
+        req.hoge = 'hoge';
+        /* eslint-enable no-param-reassign */
+        next();
+      });
+
+      it('should be called before routing', async () => {
+        app.get('/', (req, res) => {
+          assert.strictEqual(req.hoge, 'hoge');
+          res.send('Hello World!');
+        });
+        app.listen(3001, () => {});
+
+        const body = await request('http://localhost:3001');
+
+        assert.strictEqual(body, 'Hello World!');
+      });
+    });
+  });
+
+  describe('middleware', () => {
+
   });
 });
 
