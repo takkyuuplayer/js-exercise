a268edf7c9fe6e2b0bf19694c437139272efa9a8
takkyuuplayer <takkyuuplayer@gmail.com>
2017-03-28 20:10:43 +0900

    Add test for jQuery.on

--- test/jquery.spec.js
+++ test/jquery.spec.js
@@ -26,4 +26,45 @@ describe('jQuery', () => {
       ]);
     });
   });
+
+  describe('on', () => {
+    const $form = $(`
+      <form>
+        <h1>好きな色は?</h1>
+        <input type="checkbox" name="color[]" value="1" >赤
+        <input type="checkbox" name="color[]" value="2" >青
+        <input type="checkbox" name="color[]" value="3" >緑
+        <input type="checkbox" class="exclude" name="color[]" value="99" >この中にはない
+        <input type="text" name="password" value="password">
+      </form>`);
+
+    const observe = ($target) => {
+      $target.find('input.exclude').on('change', (e) => {
+        $target.find('input[type="checkbox"]').each((idx, el) => {
+          if (e.currentTarget === el) {
+            return;
+          }
+          $(el).prop('disabled', true);
+        });
+      });
+    };
+
+    it('can add event', () => {
+      observe($form);
+
+      assert.strictEqual(0, $form.find('input[disabled]').length);
+
+      $form.find('input.exclude').prop('checked', true).trigger('change');
+
+      assert.strictEqual(3, $form.find('input[disabled]').length);
+      assert.strictEqual(`<form>
+        <h1>好きな色は?</h1>
+        <input type="checkbox" name="color[]" value="1" disabled="">赤
+        <input type="checkbox" name="color[]" value="2" disabled="">青
+        <input type="checkbox" name="color[]" value="3" disabled="">緑
+        <input type="checkbox" class="exclude" name="color[]" value="99">この中にはない
+        <input type="text" name="password" value="password">
+      </form>`, $form.prop('outerHTML'));
+    });
+  });
 });
