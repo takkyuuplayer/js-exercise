6f57a688b86cb24203b9b55aa77c1472e779efba
takkyuuplayer <takkyuuplayer@gmail.com>
2017-06-13 17:50:28 +0900

    Add jsonwebtoken

---
+++ test/jsonwebtoken.spec.js
@@ -0,0 +1,21 @@
+import assert from 'power-assert';
+
+import jwt from 'jsonwebtoken';
+
+describe('jsonwebtoken', () => {
+  describe('.sign', () => {
+    it('should sign object with iat', () => {
+      const token = jwt.sign({ foo: 'bar' }, 'secret');
+      const decoded = jwt.verify(token, 'secret');
+
+      assert.strictEqual(decoded.foo, 'bar');
+      assert(decoded.iat);
+    });
+  });
+  describe('.verify', () => {
+    it('should throw exception when token is invalid', () => {
+      const token = jwt.sign({ foo: 'bar' }, 'secret');
+      assert.throws(() => { jwt.verify(token, 'wrong') });
+    });
+  });
+});
